f69579d56de48ad2f35b7cf5aace4123ffa9b7c6
Mufti <dev-null@prehistoric.me>
2015-11-25 08:10:33 +0000

    seed database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Product::class, function(Faker\Generator $f){
+    return [
+        'name' => $f->numerify("P-###"),
+        'code' => $f->numerify("Product-###"),
+        'description' => $f->sentence($nbWords = 3)
+    ];
+});
