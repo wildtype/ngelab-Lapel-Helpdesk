e6708d4878dcb4bcaa11c5792e4e8dc097cdd09f
Mufti <dev-null@prehistoric.me>
2015-11-26 08:33:56 +0000

    add acceptance test ProductList

---
+++ tests/acceptance/ProductListCept.php
@@ -0,0 +1,7 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('see list of products');
+$I->amOnPage('/product');
+$I->see('Product List');
+$I->see('Code-86');
+$I->see('Lapan anam situasi aman, ganti!');
