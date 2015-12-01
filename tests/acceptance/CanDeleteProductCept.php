584fc3ed6dba28a2172a7ccf2fad694a8fdebf41
Mufti <dev-null@prehistoric.me>
2015-12-01 03:31:06 +0000

    tambah test untuk READ

--- tests/acceptance/CanDeleteProductCept.php
+++ tests/acceptance/CanDeleteProductCept.php
@@ -1,11 +1,16 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Create, Edit, and Delete Product');
+$I->wantTo('Create, Read, Update, and Delete Product');
 $I->amOnPage('/product/create');
 $I->fillField('name', 'nama produk untuk dihapus lagi');
 $I->fillField('code', 'Code-009');
 $I->fillField('description', 'Ini nanti dihapus');
 $I->click('Submit');
+$I->see('nama produk untuk dihapus lagi');
+$I->amOnPage('/product');
+$I->click('Code-009');
+$I->seeCurrentUrlMatches('~$/product/(\d+)~');
+$I->see('View Product Code-009');
 $I->see('nama produk untuk dihapus lagi');
 $I->amOnPage('/product');
 $I->seeElement('input',['id' => 'delete-Code-009']);
